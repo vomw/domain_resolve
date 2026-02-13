b24a6d385e641fc097f9e63c02168ce3911e31f6
vomw <22770640+vomw@users.noreply.github.com>
2026-02-14 07:25:47 +0800

    update

--- resolve_domain.py
+++ resolve_domain.py
@@ -1,22 +1,59 @@
-# import msvcrt
+import msvcrt
 import os
 import random
 import subprocess
 import sys
-# import threading
+import threading
 import time
 
 
+DNS_SERVER_LIST = [
+    "138.199.149.249:53",
+    "149.112.112.10:53",
+    "149.112.112.11:53",
+    "149.112.112.112:53",
+    "149.112.112.12:53",
+    "149.112.112.9:53",
+    "176.9.1.117:53",
+    "176.9.93.198:53",
+    "185.228.168.9:53",
+    "185.228.169.9:53",
+    "208.67.220.220:443",
+    "208.67.220.222:443",
+    "208.67.222.220:443",
+    "208.67.222.222:443",
+    "49.12.222.213:53",
+    "49.12.223.2:53",
+    "49.12.43.208:53",
+    "49.12.67.122:53",
+    "78.47.71.194:53",
+    "8.8.4.4:53",
+    "8.8.8.8:53",
+    "88.198.122.154:53",
+    "9.9.9.10:53",
+    "9.9.9.11:53",
+    "9.9.9.12:53",
+    "9.9.9.9:53",
+    "91.99.154.175:53",
+]
+
+DNS_SERVER_INDEX = 0
+
+random.shuffle(DNS_SERVER_LIST)
+
+
+def get_dns_server():
+    global DNS_SERVER_INDEX
+    dns_server = DNS_SERVER_LIST[DNS_SERVER_INDEX]
+    DNS_SERVER_INDEX += 1
+    if DNS_SERVER_INDEX == len(DNS_SERVER_LIST):
+        DNS_SERVER_INDEX = 0
+    return dns_server
+
+
 def dig_query(qname, qtype):
-    dns_server = random.choice(
-        [
-            "208.67.220.220",
-            "208.67.220.222",
-            "208.67.222.220",
-            "208.67.222.222",
-        ]
-    )
-    command = f" dig +short -t{qtype} {qname} @{dns_server} -p443 "
+    dns_server_ip, dns_server_port = get_dns_server().rsplit(":", maxsplit=1)
+    command = f" dig +short -t{qtype} {qname} @{dns_server_ip} -p{dns_server_port} "
     try:
         resp = subprocess.check_output(command, shell=True).decode("utf-8").strip()
     except subprocess.CalledProcessError as e:
@@ -51,21 +88,21 @@ def main():
         print(resp)
 
 
-# def get_key_listener():
-#     """Thread function to listen for key presses."""
-#     while True:
-#         if msvcrt.kbhit():
-#             key = msvcrt.getch()
-#             if key in (b"\x00", b"\xe0"):
-#                 msvcrt.getch()
-#                 continue
-#             if key in (b"q", b"Q"):
-#                 print("\nExiting...")
-#                 os._exit(1)
-#         time.sleep(1)
+def get_key_listener():
+    """Thread function to listen for key presses."""
+    while True:
+        if msvcrt.kbhit():
+            key = msvcrt.getch()
+            if key in (b"\x00", b"\xe0"):
+                msvcrt.getch()
+                continue
+            if key in (b"q", b"Q"):
+                print("\nExiting...")
+                os._exit(1)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
-    # key_thread = threading.Thread(target=get_key_listener, daemon=True)
-    # key_thread.start()
+    key_thread = threading.Thread(target=get_key_listener, daemon=True)
+    key_thread.start()
     main()
